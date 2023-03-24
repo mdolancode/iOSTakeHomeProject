acd1af6be142ef8d770762541a09bc40f6653cc5
Matthew Dolan <mdolancode@gmail.com>
2023-03-23 20:53:49 -0700

    Initial Commit

---
+++ iOSTakeHomeProject/iOSTakeHomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSTakeHomeProjectApp.swift
+//  iOSTakeHomeProject
+//
+//  Created by Matthew Dolan on 2023-03-23.
+//
+
+import SwiftUI
+
+@main
+struct iOSTakeHomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
