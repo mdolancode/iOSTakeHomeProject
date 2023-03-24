292f2d77b61d338a98645529e6ae8afc5ff7cf11
Matthew Dolan <mdolancode@gmail.com>
2023-03-23 21:17:36 -0700

    Create dark and light colours and theme.

---
+++ iOSTakeHomeProject/Resources/Theme.swift
@@ -0,0 +1,15 @@
+//
+//  Theme.swift
+//  iOSTakeHomeProject
+//
+//  Created by Matthew Dolan on 2023-03-23.
+//
+
+import SwiftUI
+
+enum Theme {
+    static let background = Color("background")
+    static let detailBackground = Color("detail-background")
+    static let text = Color("text")
+    static let pill = Color("pill")
+}
