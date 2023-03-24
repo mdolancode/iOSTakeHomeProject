20355b12bc811d79807b387734d3630174327d2f
Matthew Dolan <mdolancode@gmail.com>
2023-03-23 21:16:57 -0700

    Create Symbols enum.

---
+++ iOSTakeHomeProject/Resources/Symbols.swift
@@ -0,0 +1,14 @@
+//
+//  Symbols.swift
+//  iOSTakeHomeProject
+//
+//  Created by Matthew Dolan on 2023-03-23.
+//
+
+import SwiftUI
+
+enum Symbols {
+    static let person = Image(systemName: "person.2")
+    static let gear = Image(systemName: "gear")
+    static let plus = Image(systemName: "plus")
+}
