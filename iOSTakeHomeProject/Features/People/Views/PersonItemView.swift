aa66f6834cd4fac318d138df6f978d68c800e2a5
Matthew Dolan <mdolancode@gmail.com>
2023-03-25 17:26:03 -0700

    Used local JSON to hook up data for People View.

--- iOSTakeHomeProject/Features/People/Views/PersonItemView.swift
+++ iOSTakeHomeProject/Features/People/Views/PersonItemView.swift
@@ -9,20 +9,27 @@ import SwiftUI
 
 struct PersonItemView: View {
     
-    let user: Int
+    let user: User
     
     var body: some View {
         
         VStack(spacing: .zero) {
-            Rectangle()
-                .fill(.blue)
-                .frame(height: 130)
+            AsyncImage(url: .init(string: user.avatar)) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(height: 130)
+                    .clipped()
+            } placeholder: {
+                ProgressView()
+            }
+
             
             VStack(alignment: .leading) {
                 
-                PillView(id: user)
+                PillView(id: user.id)
                 
-                Text("<First name> <Last name>")
+                Text("\(user.firstName) \(user.lastName)")
                     .foregroundColor(Theme.text)
                     .font(
                         .system(.body, design: .rounded)
@@ -44,8 +51,14 @@ struct PersonItemView: View {
 }
 
 struct PersonItemView_Previews: PreviewProvider {
+    static var previewUser: User {
+        let users = try! StaticJSONMapper.decode(file: "UsersStaticData",
+                                                 type: UsersResponse.self)
+        return users.data.first!
+    }
+    
     static var previews: some View {
-        PersonItemView(user: 0)
+        PersonItemView(user: previewUser)
             .frame(width: 250)
     }
 }
